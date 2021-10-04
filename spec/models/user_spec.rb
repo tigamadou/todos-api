87f49c0e9dd9085b1f88c48c212f33d9e1b7b7c0
Amadou <amadou140589@gmail.com>
2021-10-04 19:48:56 +0100

    user model and spec added

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  # Association test
+  # ensure User model has a 1:n relationship with the Todo model
+  it { should have_many(:todos) }
+
+  # Validation test
+  # ensure name, email and password_digest are present before save
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+end
